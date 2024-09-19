a84506f0ef082d61f7d26f5b67c55869964bb5a8
juanestev <juanchiestevez2006@gmail.com>
2024-09-19 17:11:15 -0300

    feat: subiendo conexión a la base de datos

---
+++ Códigos/Sistema de puntos/php/conexion_be.php
@@ -0,0 +1,15 @@
+<?php
+
+    $conexion = mysqli_connect("sql308.infinityfree.com	
+", "if0_37344228", "Juanynico1020", "if0_37344228_Ecosorter");
+
+    /*
+    if($conexion){
+        echo 'Conectado exitosamente a la Base de Datos';
+    }else{
+        echo'No se ha podido conectar a la Base de Datos';
+    }
+    */
+
+    
+?>
