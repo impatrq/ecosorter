c83fd601d39ebb3e46bba41f463a3a7fd455ed33
juanestev <juanchiestevez2006@gmail.com>
2024-10-04 16:12:20 -0300

    feat: agregando suma de 10 puntos

---
+++ Códigos/Sistema de puntos/php/login_suma10_usuario_be.php
@@ -0,0 +1,121 @@
+<?php
+
+    session_start();
+
+    include 'conexion_be.php';
+
+    $correo = $_POST['correo'];
+    $contrasena = $_POST['contrasena'];
+    $contrasena = hash('sha512', $contrasena);
+
+
+    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' and contrasena='$contrasena' ");
+
+    if(mysqli_num_rows($validar_login) > 0){
+        $row = mysqli_fetch_assoc($validar_login);
+        
+        $_SESSION['usuario'] = $correo;
+        $_SESSION['puntos'] = $row['puntos'];
+        $_SESSION['puntos'] = $_SESSION['puntos'] + 10;
+        header("location: ../bienvenida.php");
+        exit;
+    }else{
+        echo '
+            <script>
+                alert("Usuario no existe, por favor verifique los datos introducidos");
+                window.location = "../suma10puntos.php";
+            </script>
+        ';
+        exit;
+    }
+?><?php
+
+    session_start();
+
+    include 'conexion_be.php';
+
+    $correo = $_POST['correo'];
+    $contrasena = $_POST['contrasena'];
+    $contrasena = hash('sha512', $contrasena);
+
+
+    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' and contrasena='$contrasena' ");
+
+    if(mysqli_num_rows($validar_login) > 0){
+        $row = mysqli_fetch_assoc($validar_login);
+        
+        $_SESSION['usuario'] = $correo;
+        $_SESSION['puntos'] = $row['puntos'];
+        $_SESSION['puntos'] = $_SESSION['puntos'] + 10;
+        header("location: ../bienvenida.php");
+        exit;
+    }else{
+        echo '
+            <script>
+                alert("Usuario no existe, por favor verifique los datos introducidos");
+                window.location = "../suma10puntos.php";
+            </script>
+        ';
+        exit;
+    }
+?><?php
+
+    session_start();
+
+    include 'conexion_be.php';
+
+    $correo = $_POST['correo'];
+    $contrasena = $_POST['contrasena'];
+    $contrasena = hash('sha512', $contrasena);
+
+
+    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' and contrasena='$contrasena' ");
+
+    if(mysqli_num_rows($validar_login) > 0){
+        $row = mysqli_fetch_assoc($validar_login);
+        
+        $_SESSION['usuario'] = $correo;
+        $_SESSION['puntos'] = $row['puntos'];
+        $_SESSION['puntos'] = $_SESSION['puntos'] + 10;
+        header("location: ../bienvenida.php");
+        exit;
+    }else{
+        echo '
+            <script>
+                alert("Usuario no existe, por favor verifique los datos introducidos");
+                window.location = "../suma10puntos.php";
+            </script>
+        ';
+        exit;
+    }
+?><?php
+
+    session_start();
+
+    include 'conexion_be.php';
+
+    $correo = $_POST['correo'];
+    $contrasena = $_POST['contrasena'];
+    $contrasena = hash('sha512', $contrasena);
+
+
+    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' and contrasena='$contrasena' ");
+
+    if(mysqli_num_rows($validar_login) > 0){
+        $row = mysqli_fetch_assoc($validar_login);
+        
+        $_SESSION['usuario'] = $correo;
+        $_SESSION['puntos'] = $row['puntos'];
+        $_SESSION['puntos']++;
+        header("location: ../bienvenida.php");
+        exit;
+    }else{
+        echo '
+            <script>
+                alert("Usuario no existe, por favor verifique los datos introducidos");
+                window.location = "../suma10puntos.php";
+            </script>
+        ';
+        exit;
+    }
+?>
