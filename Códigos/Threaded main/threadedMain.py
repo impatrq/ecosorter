0a09fc58b53b04dded6c38aac432289453c047f6
juanestev <juanchiestevez2006@gmail.com>
2024-10-14 00:53:03 -0300

    Feat: Subiendo threaded main

---
+++ Códigos/Threaded main/threadedMain.py
@@ -0,0 +1,260 @@
+from ultralytics import YOLO
+from enum import Enum
+from gpiozero import DistanceSensor
+import RPi.GPIO as GPIO
+from flask import Flask, render_template, url_for
+import threading
+from time import sleep
+
+#configurando GPIO
+GPIO.setmode(GPIO.BCM)
+
+#Variable para flask
+app = Flask(__name__)
+
+#Sensores Ultrasónicos
+medidorVid = DistanceSensor(echo= 24, trigger= 23)
+medidorPlas = DistanceSensor(echo= 25, trigger= 8)
+medidorMet = DistanceSensor(echo= 7, trigger= 12)
+medidorRes = DistanceSensor(echo= 16, trigger= 20)
+
+#capacitivo
+capacitivo = 20
+GPIO.setup(capacitivo, GPIO.IN)
+
+# Declaro las variables para poder mostrar que objeto detectó
+varPlas = 0
+varMet = 0
+varVid = 0
+varRes = 0
+
+# Configuración de los pines GPIO del motor para el movimiento de los ejes 
+PUL_PIN_1 = 17  # Pin para el pulso 
+DIR_PIN_1 = 27  # Pin para la dirección 
+GPIO.setup(PUL_PIN_1, GPIO.OUT)
+GPIO.setup(DIR_PIN_1, GPIO.OUT)
+
+# Configuración de los pines GPIO del motor para la apertura de la compuerta del canasto
+PUL_PIN_2 =  2
+DIR_PIN_2 = 3
+GPIO.setup(PUL_PIN_2, GPIO.OUT)
+GPIO.setup(DIR_PIN_2, GPIO.OUT)
+
+class ModelType(Enum):
+    YOLOv8n = 'yolov8n.pt'
+    YOLOv8s = 'yolov8s.pt'
+    pers = 'ecosorter.pt'
+
+class Camera(Enum):
+    LAPTOP = '0'
+
+def mover_motor_eje(pasos, direccion, velocidad):
+    # Establecer la dirección
+    GPIO.output(DIR_PIN_1, direccion)
+    
+    for paso in range(pasos):
+        # Enviar un pulso al driver
+        GPIO.output(PUL_PIN_1, GPIO.HIGH)
+        sleep(velocidad)  # Controla la velocidad del motor
+        GPIO.output(PUL_PIN_1, GPIO.LOW)        
+        sleep(velocidad)
+
+def mover_motor_canasto(pasos, direccion, velocidad):
+    # Establecer la dirección
+    GPIO.output(DIR_PIN_2, direccion)
+    
+    for paso in range(pasos):
+        # Enviar un pulso al driver
+        GPIO.output(PUL_PIN_2, GPIO.HIGH)
+        sleep(velocidad)  # Controla la velocidad del motor
+        GPIO.output(PUL_PIN_2, GPIO.LOW)        
+        sleep(velocidad)
+
+#funcion de medicion
+class medir:
+    def vid(self):
+            try:
+                disvid = medidorVid.distance * 100
+                sleep(0.1)
+            except:
+                print("error")
+            print (disvid)
+            disvid = (disvid / 40) * 100
+            capacidadvid = 100 - disvid
+            self.capacidadvid = capacidadvid
+            return self.capacidadvid
+
+    def plas(self):
+        try:
+            displas = medidorPlas.distance * 100
+            sleep(.01)
+        except:
+            print("error")
+        displas = (displas * 100) / 40
+        capacidadplas = 100 - displas
+        self.capacidadplas = capacidadplas
+        return self.capacidadplas 
+
+    def res(self):
+        try:
+            disres = medidorRes.distance * 100 
+            sleep(.01)
+        except:
+            print("error")
+        disres = (disres * 100) / 40
+        capacidadres = 100 - disres
+        self.capacidadres = capacidadres 
+        return self.capacidadres 
+
+    def met(self):
+        try:
+            dismet = medidorMet.distance * 100 
+            sleep(.01)
+        except:
+            print("error")
+        dismet = (dismet * 100) / 40
+        capacidadmet = 100 - dismet 
+        self.capacidadmet = capacidadmet
+        return self.capacidadmet
+                  
+#Función detectar objetos
+def liveObjectDetection(modelType:ModelType):
+    funcionamiento = 0
+    
+    global varMet
+    global varPlas
+    global varRes
+    global varVid
+
+    model = YOLO(modelType.value)
+    
+    # Usar 'stream=True' para manejar largos streams
+    results = model.predict(source=Camera.LAPTOP.value, stream=True, show=True)
+    
+
+    for result in results:
+        # Acceder a los nombres de las clases detectadas en los resultados
+        for box in result.boxes:
+            # Obtener el ID de la clase detectada
+            class_id = int(box.cls[0])  
+            class_name = model.names[class_id] 
+
+            if GPIO.input(capacitivo) == 0:
+                if class_name == 'botella' and funcionamiento == 0:  
+                    #Bloqueo la tarea
+                    funcionamiento = 1
+                    print("Se ha detectado una botella!")
+                    # Muevo el canasto al cesto de plástico
+                    mover_motor_eje(2000, GPIO.HIGH, 0.001)  
+
+                    # Pausa antes de cambiar de dirección
+                    sleep(1)
+                    
+                    #Abro canasto 
+                    mover_motor_canasto(120, GPIO.HIGH, 0.001)
+
+                    sleep(1)
+
+                    #Cierro canasto
+                    mover_motor_canasto(120, GPIO.LOW, 0.001)
+
+                    # Mover el motor en la otra dirección
+                    mover_motor_eje(2000, GPIO.LOW, 0.001) 
+
+                    #Indico al servidor que objeto detecté
+                    varPlas = 1
+                    #Libero la tarea 
+                    funcionamiento = 0
+
+                elif class_name == 'envoltorio' and funcionamiento == 0:  
+                    #Bloqueo tarea
+                    funcionamiento = 1
+                    print("Se ha detectado un metal!")
+                    # Muevo el canasto al cesto de metales
+                    mover_motor_eje(2000, GPIO.LOW, 0.001)  
+
+                    # Pausa antes de cambiar de dirección
+                    sleep(1)
+                    
+                    #Abro canasto 
+                    mover_motor_canasto(120, GPIO.HIGH, 0.001)
+
+                    sleep(1)
+
+                    #Cierro canasto
+                    mover_motor_canasto(120, GPIO.LOW, 0.001)
+
+                    # Mover el motor en la otra dirección
+                    mover_motor_eje(2000, GPIO.HIGH, 0.001) 
+
+                    #Indico al servidor que objeto detecté
+                    varMet = 1
+                    #Libero tarea
+                    funcionamiento = 0
+
+                else:
+                    print(f"Se ha detectado un objeto: {class_name}")
+
+#Defino funcion para interfaz gráfica 
+def servidor():
+        
+    @app.route("/")
+    def index():
+            return render_template("index.html")
+
+    @app.route("/medidas")
+    def medidas():
+            global varVid
+            global varMet
+            global varPlas
+            global varRes
+
+            while True:
+                    refrescar = 5
+                    vid = int(class_.vid())
+                    plas = int(class_.plas())
+                    res = int(class_.res())
+                    met = int(class_.met())
+                    detPlas = 0
+                    detMet = 0
+                    detRes = 0
+                    detVid = 0
+                    if varPlas == 1:
+                         detPlas = 1
+                         refrescar = 15
+                         varPlas = 0
+                    elif varMet == 1:
+                         detMet = 1
+                         refrescar = 15
+                         varMet = 0
+                    elif varRes == 1:
+                         detRes = 1
+                         refrescar = 15
+                         varRes = 0
+                    elif varVid == 1:
+                         detVid == 1
+                         refrescar = 15
+                         varVid = 0
+                    return render_template("medidas.html",
+                                            vid = vid,
+                                            plas = plas,
+                                            res = res,
+                                            met = met,
+                                            detPlas = detPlas,
+                                            detMet = detMet,
+                                            detRes = detRes,
+                                            detVid = detVid,
+                                            refrescar = refrescar)
+
+                         
+
+if __name__ == '__main__':
+    class_ = medir()
+    #Creo los hilos
+    hiloDeteccion = threading.Thread(target= liveObjectDetection(ModelType.YOLOv8n))
+    hiloInterfaz = threading.Thread(target= servidor)
+
+    #Los inicio
+    hiloDeteccion.start()
+    hiloInterfaz.start()
+
